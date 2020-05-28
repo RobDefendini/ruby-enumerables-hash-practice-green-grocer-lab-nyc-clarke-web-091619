35911fb1176615fda76db656c7bb7bcdc06ac06c
Robert Defendini <robdefendini@gmail.com>
2020-05-28 21:58:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -51,8 +51,8 @@ end
 ####
 
 def checkout(cart, coupons)
-    consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+    cc = consolidate_cart(cart)
+  cwca = apply_coupons(cc, coupons)
   cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
 
   total = 0.0
